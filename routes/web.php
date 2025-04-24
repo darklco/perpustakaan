aab4d560c616b2c54cc2cc23be1402e941b2bcd0
darklco <darkvlcov@gmail.com>
2025-04-24 12:53:00 +0700

    membuat sistem login register

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,40 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PeminjamanController;
 use App\Http\Controllers\AdminDashboardController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route Register
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register.form');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+// Route Login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+// Route Logout
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Route yang hanya bisa diakses oleh user yang sudah login
 Route::middleware(['auth'])->group(function () {
+
+    // Route untuk pengguna biasa
     Route::get('/dashboard-user', [BookController::class, 'index']);
     Route::post('/buku', [BookController::class, 'store']);
-
     Route::post('/pinjam', [PeminjamanController::class, 'pinjam']);
     Route::post('/kembalikan', [PeminjamanController::class, 'kembalikan']);
 
     Route::resource('kategori', CategoryController::class);
 
-    // Tambahkan blok middleware admin DI DALAM middleware auth
+    // Route khusus admin
     Route::middleware(['admin'])->group(function () {
         Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
     });
